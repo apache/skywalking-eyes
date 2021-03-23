2aeae025a4f68bb4c8ac1cac463ecd47b60f946c
kezhenxu94 <kezhenxu94@apache.org>
2021-03-23 10:47:29 +0800

    feature: support .rst files and allow fixing header of a single file

--- commands/header_fix.go
+++ commands/header_fix.go
@@ -34,13 +34,18 @@ var FixCommand = &cobra.Command{
 	Long:    "fix command walks the specified paths recursively and fix the license header if the specified files don't have the license header.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		var result header.Result
+		var errors []string
+		var files []string
 
-		if err := header.Check(&Config.Header, &result); err != nil {
+		if len(args) > 0 {
+			files = args
+		} else if err := header.Check(&Config.Header, &result); err != nil {
 			return err
+		} else {
+			files = result.Failure
 		}
 
-		var errors []string
-		for _, file := range result.Failure {
+		for _, file := range files {
 			if err := header.Fix(file, &Config.Header, &result); err != nil {
 				errors = append(errors, err.Error())
 			}

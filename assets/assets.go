126ce2b4360bce2f05d87344736f4b17e6e90f31
Youhan Wu <43005566+zooltd@users.noreply.github.com>
2021-07-17 01:21:39 +0800

    Fix compatibility issues in Windows (#44)

--- assets/assets.go
+++ assets/assets.go
@@ -21,15 +21,16 @@ package assets
 import (
 	"embed"
 	"io/fs"
+	"path/filepath"
 )
 
 //go:embed *
 var assets embed.FS
 
 func Asset(file string) ([]byte, error) {
-	return assets.ReadFile(file)
+	return assets.ReadFile(filepath.ToSlash(file))
 }
 
 func AssetDir(dir string) ([]fs.DirEntry, error) {
-	return assets.ReadDir(dir)
+	return assets.ReadDir(filepath.ToSlash(dir))
 }
